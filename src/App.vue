e4c6483b3ae7bca5f681b7edb9064549b157f5c2
Karl Querel <karlquerel@gmail.com>
2025-02-08 17:46:37 +0100

    set up Node, VITE and some frontend elements

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Welcome to My Portfolio</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+/* You can add styles here */
+</style>
