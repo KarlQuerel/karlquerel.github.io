4f3f685c6dd56c19feb93ac4b37ca7e7e8484bc5
Karl Querel <99736142+KarlQuerel@users.noreply.github.com>
2025-02-17 20:38:58 +0100

    frontend needs to be fixed

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,48 @@
 <template>
-  <div>
-    <!-- Background Animation (Particles) -->
-    <div id="particles-js"></div>
+	<div id="app">	<!-- Centralized container -->
+	<!-- Background Animation -->
+	<div id="particles-js"></div>
 
-    <!-- Under Construction Message -->
-    <div>
-      <h1>Under Construction</h1>
-      <p>This site is currently being built. Stay tuned for more!</p>
-    </div>
+	<!-- Main Content -->
+	<div class="content">
+		<h1>Under Construction</h1>
+		<p>This site is currently being built. Stay tuned for more!</p>
 
-    <!-- Social Buttons Section -->
-    <div class="social-buttons">
-      <div class="share">
-        <a href="mailto:karlquerel@gmail.com" target="_blank">
-          <i class="nes-icon gmail is-large"></i>
-        </a>
-        <a href="https://github.com/KarlQuerel" target="_blank">
-          <i class="nes-icon github is-large"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/karlquerel" target="_blank">
-          <i class="nes-icon linkedin is-large"></i>
-        </a>
-      </div>
-    </div>
+		<!-- Social Buttons -->
+		<div class="share">
+		<a href="mailto:karlquerel@gmail.com" target="_blank">
+			<i class="nes-icon gmail is-large"></i>
+		</a>
+		<a href="https://github.com/KarlQuerel" target="_blank">
+			<i class="nes-icon github is-large"></i>
+		</a>
+		<a href="https://www.linkedin.com/in/karlquerel" target="_blank">
+			<i class="nes-icon linkedin is-large"></i>
+		</a>
+		</div>
 
-    <!-- Button to Trigger Click Count -->
-    <button id="clicker" class="nes-btn is-error" @click="incrementCounter">
-      CLICK ME
-    </button>
-    <p>How many brave souls have summoned their mighty index finger?</p>
-    <p><span>{{ counter }}</span></p>
-  </div>
+		<!-- Clicker Button -->
+		<button id="clicker" class="nes-btn is-error" @click="incrementCounter">
+		CLICK ME
+		</button>
+		<p>How many brave souls have summoned their mighty index finger?</p>
+		<p><span>{{ counter }}</span></p>
+	</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data() {
-    return {
-      counter: 0
-    };
-  },
-  methods: {
-    incrementCounter() {
-      this.counter += 1;
-    }
-  }
+	name: "App",
+	data() {
+	return {
+		counter: 0
+	};
+	},
+	methods: {
+	incrementCounter() {
+		this.counter += 1;
+	}
+	}
 };
 </script>
-
-<style scoped>
-/* You can add custom styles for this component here */
-</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/***	IMPORTS		***/
 import './styles/main.scss';
 import { createApp } from 'vue';
 import App from './App.vue';
