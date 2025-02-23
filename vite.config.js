afe10ad1f0140c7f54547acbbd25297c58e8078b
Karl Querel <99736142+KarlQuerel@users.noreply.github.com>
2025-02-23 02:28:22 +0100

    Add dist folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/',
+  base: '/', 
   plugins: [vue()],
   root: '.',
   build: {
     outDir: 'dist',
   },
   publicDir: 'public',
-})
+  resolve: {
+    alias: {
+      // Ensure Vue is correctly resolved
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+  },
+});
