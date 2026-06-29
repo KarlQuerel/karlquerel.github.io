cc01de87081c0fe828fe8915c620f9517d0be90c
Karl Querel <karlquerel@gmail.com>
2026-06-29 02:30:20 +0200

    feat: wrap navbar links on narrow screens, tweak nav and terminal copy
    Let nav links wrap onto a second row instead of horizontal-scrolling,
    rename the Terminal link to 'for the nerds', and tidy terminal copy.

--- src/components/SiteNavbar.vue
+++ src/components/SiteNavbar.vue
@@ -3,7 +3,7 @@
 		<div class="site-chrome-links">
 			<router-link to="/" class="site-chrome-link">Home</router-link>
 			<router-link to="/about" class="site-chrome-link">About</router-link>
-			<router-link to="/terminal" class="site-chrome-link">Terminal</router-link>
+			<router-link to="/terminal" class="site-chrome-link">for the nerds</router-link>
 			<router-link to="/game" class="site-chrome-link">Game</router-link>
 		</div>
 	</nav>
