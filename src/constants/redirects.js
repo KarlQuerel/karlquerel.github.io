0b5a91c3dd86861611f1b8d39853e6724a1dafed
Karl Querel <karlquerel@gmail.com>
2026-09-24 16:23:50 +0200

    refactor(redirects): add the redirect notice text

--- src/constants/redirects.js
+++ src/constants/redirects.js
@@ -2,3 +2,6 @@
 
 // The /secret_link easter egg.
 export const RICKROLL_URL = 'https://shattereddisk.github.io/rickroll/rickroll.mp4'
+
+// What the page shows for the instant before the browser leaves.
+export const REDIRECT_NOTICE = 'Redirecting...'
